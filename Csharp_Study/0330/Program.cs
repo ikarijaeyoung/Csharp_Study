74dfba96d67dedc328096464bc1fa25a282029f5
정재영 <wodud04277@gmail.com>
2025-03-30 19:10:56 +0900

    Try_Lottery
    랜덤숫자 1회 설정후, 유저가 그 숫자를 입력해야 끝나는 프로그램

--- Csharp_Study/0330/Program.cs
+++ Csharp_Study/0330/Program.cs
@@ -2,9 +2,52 @@
 {
     internal class Program
     {
+        public class Lottery
+        {
+            // 필드
+            // 당첨 번호
+            // 입력 번호
+            // 당첨 번호를 뽑을 랜덤 객채?
+
+            private int winNum;
+            public int userInput;
+            private Random ranNum = new Random();
+
+            public void SetWinNum()
+            {
+                winNum = ranNum.Next(1, 12);
+            }
+
+            public void TryLottery()
+            {
+                Console.Write("1 ~ 12 숫자를 입력해 주세요 : ");
+
+                while (true)
+                {
+                    userInput = int.Parse(Console.ReadLine());
+                    if (userInput == winNum)
+                    {
+                        Console.WriteLine(" 당첨! ");
+                        break;
+                    }
+                    else { Console.WriteLine("다시 입력해주세요."); }
+                }
+            }
+
+            // 매서드
+            // 당첨 번호를 랜덤으로 설정
+            // 그 번호와 입력된 번호가 일치하는지 판별
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Github desktop 테스트 입니다.");
+            Console.WriteLine("랜덤한 숫자와 내가 입력한 숫자가 일치하는지 테스트");
+
+            Lottery lottery = new();
+
+            lottery.SetWinNum();
+            lottery.TryLottery();
+
         }
     }
 }
