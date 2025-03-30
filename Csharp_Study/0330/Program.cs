6cff9aa94667ba771d0d0beccecfd989284bba6d
정재영 <wodud04277@gmail.com>
2025-03-30 18:29:41 +0900

    Test
    test

---
+++ Csharp_Study/0330/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _0330
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Github desktop 테스트 입니다.");
+        }
+    }
+}
