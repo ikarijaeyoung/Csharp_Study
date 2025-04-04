5b69fe1e9ae4eb147c697d057a4ed3999056c6aa
정재영 <wodud04277@gmail.com>
2025-04-04 15:18:07 +0900

    Set
    중간 점검

---
+++ Csharp_Study/0404_Pattern_Practice/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _0404_Pattern_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    
+    
+    public class Weapon
+    {
+        string name;
+        int damage;
+        float range;
+    }
+}
